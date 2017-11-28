0d7545a85bb7a64659368560c40ba8d5031f6cd9
HKR-JohnDGroups <33514450+HKR-JohnDGroups@users.noreply.github.com>
2017-11-28 19:18:57 +0100

    Add files via upload

---
+++ Rasmus the Game/core/assets/ddede.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="inside" tilewidth="16" tileheight="16" tilecount="1645" columns="47">
+ <image source="inside.png" width="752" height="560"/>
+</tileset>
